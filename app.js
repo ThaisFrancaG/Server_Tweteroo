af2cddceab24c15598215fe378798ef080487dab
Thais <tfrancag@gmail.com>
2022-01-23 11:13:34 -0300

    Alguns commit foram perdidos devido a inicializaçào incorreta de branches. Tarefas realizadas até o momento: Adicionados servidores para sign-up e login. Adicionadas arrays globais;Post Sign-Up está armazenando dados em array global enquanto terminal corre;Post e Get funcionando corretamente. Falta adicionar condições para evitar duplicatas e conectar ao front-end

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+const userInfo = [];
+const tweetContent = [];
+
+app.post("/sign-up",(req,res)=>{
+    const user = req.body;
+    userInfo.push(user);
+    res.send("OK");
+});
+
+app.post("/tweets",(req,res)=>{
+    const tweet = req.body;
+    tweetContent.push(tweet);
+    res.send("OK")
+})
+
+app.get("/tweets",(req,res)=>{
+    res.send(tweetContent.slice(-10));
+   
+})
 
 
 app.listen(5000,()=> console.log("Server Running"));
